7e5be7d7ba167dc9576bd79e0d18a0c868beb8ac
Renan Germano <renangermano@MacBook-Pro-de-Renan-2.local>
2021-11-18 16:50:37 -0300

    Criando documentação do repositório (README) mostrando estrutura do objeto receita.

--- src/paginas/ListaReceitas/ListaReceitas.js
+++ src/paginas/ListaReceitas/ListaReceitas.js
@@ -2,28 +2,6 @@
 import Receita from "../../componentes/Receita/Receita";
 import "./ListaReceitas.css";
 
-const receita = {
-    id: 1,
-    nome: "Nome da receita",
-    descricao: "Descrição da receita",
-    imagem: "https://img.itdg.com.br/tdg/images/blog/uploads/2017/07/shutterstock_413580649-300x200.jpg",
-    categoria: "Sobremesa",
-    tempo: "2 horas",
-    porcoes: 6,
-    ingredientes: [
-        "Ingrediente 1",
-        "Ingrediente 2",
-        "Ingrediente 3"
-    ],
-    preparacao: [
-        "Passo 1", 
-        "Passo 2",
-        "Passo 3", 
-        "Passo 4",
-        "Passo 5"
-    ]
-}
-
 const receitas = [
     {
         id: 1,
